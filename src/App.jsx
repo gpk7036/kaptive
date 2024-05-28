b687c60c40946618cb54a7e44b1e8ed09e068cac
praveen <gpraveen957@gmail.com>
2024-05-28 06:16:16 +0530

    kaptive

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,16 @@ import "./App.css";
 import { CashFlowCharts } from "./components/CashFlowCharts";
 import { CashflowSummary } from "./components/CashflowSummary";
 import SimpleSidebar from "./components/Sidebar";
-import Data from "../db.json";
+import data from "../db.json";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("../db.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then((data) => setData(data.Sheet1))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
 
   return (
     <>
       <SimpleSidebar>
-        <CashFlowCharts data={data} />
-        <CashflowSummary data={data} />
+        <CashFlowCharts data={data.Sheet1} />
+        <CashflowSummary data={data.Sheet1} />
       </SimpleSidebar>
     </>
   );
